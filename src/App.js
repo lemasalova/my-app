5341933957462ac778e781c4d0258834c5488c68
l.masalova <test@test.test>
2022-05-12 21:38:58 +0300

    fix prettier

--- src/App.js
+++ src/App.js
@@ -1,61 +1,56 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 const Profile = () => {
-  return (
-    <h2>Это страница с профилем</h2>
-  )
-}
+  return <h2>Это страница с профилем</h2>;
+};
 
 const Messages = () => {
-  return (
-    <h2>Это страница сообщений</h2>
-  )
-}
+  return <h2>Это страница сообщений</h2>;
+};
 
 const Settings = () => {
-  return (
-    <h2>Это страница с настройками</h2>
-  )
-}
+  return <h2>Это страница с настройками</h2>;
+};
 
 const Friends = () => {
-  return (
-    <h2>Это страница друзей</h2>
-  )
-}
+  return <h2>Это страница друзей</h2>;
+};
 
 function App() {
   return (
     <div className="container-fluid mt-3">
-    <div className="row">
-    <div className="col-sm-3">
-    <div className="nav flex-column nav-pills" aria-orientation="vertical">
-   
-    <NavLink className="nav-link" to="profile">
-    Профиль
-    </NavLink>
-    <NavLink className="nav-link" to="messages">
-    Сообщения
-    </NavLink>
-    <NavLink className="nav-link" to="settings">
-    Настройки
+      <div className="row">
+        <div className="col-sm-3">
+          <div
+            className="nav flex-column nav-pills"
+            aria-orientation="vertical"
+          >
+            <NavLink className="nav-link" to="profile">
+              Профиль
+            </NavLink>
+            <NavLink className="nav-link" to="messages">
+              Сообщения
+            </NavLink>
+            <NavLink className="nav-link" to="settings">
+              Настройки
             </NavLink>
             <NavLink className="nav-link" to="friends">
-    Друзья
-    </NavLink>
-    </div>
-    </div>
+              Друзья
+            </NavLink>
+          </div>
+        </div>
         <div className="col-sm-9">
           <Routes>
-            <Route path="/profile" element={<Profile />}/>
-            <Route path="/messages" element={<Messages />}/>
-            <Route path="/settings" element={<Settings />}/>
-            <Route path="/friends" element={<Friends />}/>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/friends" element={<Friends />} />
           </Routes>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>);
+  );
 }
 
 export default App;
